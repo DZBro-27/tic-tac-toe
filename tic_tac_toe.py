200f4dce19afe28bdd244ac3c9b17407edc354c9
DZBro-27 <grimmer.27reg@gmail.com>
2026-01-29 23:48:14 +0300

    Add initial draw_board prototype

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,20 @@
 def run():  # Запуск игры
 
     print("Игра запустилась!!!")
+
+def draw_board():  # Создание игрового поля
+
+    print("Создание игрового поля...")
+
+
+# Тестовая отрисовка пустого игрового поля
+a = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+for z, i in enumerate(a):
+  for x, y in enumerate(i):
+    if x < len(i) - 1:
+      print(y + " | ", end = "")
+    else:
+      print(y, end = "")
+  if z < len(a) - 1:
+    print()
+    print("- - - - -")
