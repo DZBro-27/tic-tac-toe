67848bbfa987b516f1462fcc0efdcc22c9d02299
DZBro-27 <grimmer.27reg@gmail.com>
2026-01-30 22:56:25 +0300

    Add NxN board creation

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,19 +2,12 @@ def run():  # Запуск игры
 
     print("Игра запустилась!!!")
 
-def draw_board():  # Создание игрового поля
-
-    print("Создание игрового поля...")
-
-
-# Тестовая отрисовка пустого игрового поля
-a = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-for z, i in enumerate(a):
-  for x, y in enumerate(i):
-    if x < len(i) - 1:
-      print(y + " | ", end = "")
-    else:
-      print(y, end = "")
-  if z < len(a) - 1:
-    print()
-    print("- - - - -")
+def create_board(n: int) -> list[list[str]]:
+    """Создание игрового поля размером n x n"""
+    board = []
+    for outer in range(n):
+        row = []
+        for inner in range(n):
+          row.append(" ")
+        board.append(row)
+    return board
