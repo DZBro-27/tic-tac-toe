b4ba2103a43bb4269aadb4f74aac8f7eeac5292c
DZBro-27 <grimmer.27reg@gmail.com>
2026-02-02 20:13:47 +0300

    fix: correct draw_board rendering for NxN boards

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,6 +2,27 @@ def run():  # Запуск игры
 
     print("Игра запустилась!!!")
 
+def draw_board(board):
+    """Отрисовка игрового поля"""
+    
+    # Определяем длину строки игрового поля
+    board_size = len(board)
+    row_length = 4*board_size - 3  
+
+    # Внешний цикл проходит по строкам игрового поля
+    for row_index, row in enumerate(board):
+      
+      # Внутренний цикл проходит по ячейкам строки
+      for col_index, cell in enumerate(row):
+        if col_index < len(row) - 1:
+          print(cell + " | ", end = "")
+        else:
+          print(cell, end = "")
+          
+      if row_index < (board_size- 1):
+        print()
+        print("-" * row_length)
+
 def create_board(n: int) -> list[list[str]]:
     """Создание игрового поля размером n x n"""
     board = []
