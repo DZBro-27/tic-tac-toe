aeeba410353b6ae059ac2707167c682f3c7b84ef
DZBro-27 <grimmer.27reg@gmail.com>
2026-02-24 22:49:22 +0300

    Style: apply black formatting

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,6 @@
 """Модуль для игры в крестики-нолики."""
 
+
 def create_board(n: int) -> list[list[str]]:
     """Создание игрового поля размером n x n."""
 
@@ -7,112 +8,119 @@ def create_board(n: int) -> list[list[str]]:
     for outer in range(n):
         row = []
         for inner in range(n):
-          row.append(" ")
+            row.append(" ")
         board.append(row)
     return board
 
+
 def draw_board(board):
     """Отрисовка игрового поля."""
-    
+
     # Определяем длину строки игрового поля
     board_size = len(board)
-    row_length = 4*board_size - 3  
+    row_length = 4 * board_size - 3
 
     # Внешний цикл проходит по строкам игрового поля
     for row_index, row in enumerate(board):
-      
-      # Внутренний цикл проходит по ячейкам строки
-      for col_index, cell in enumerate(row):
-        if col_index < len(row) - 1:
-          print(cell + " | ", end = "")
+
+        # Внутренний цикл проходит по ячейкам строки
+        for col_index, cell in enumerate(row):
+            if col_index < len(row) - 1:
+                print(cell + " | ", end="")
+            else:
+                print(cell, end="")
+
+        if row_index < (board_size - 1):
+            print()
+            print("-" * row_length)
         else:
-          print(cell, end = "")
-          
-      if row_index < (board_size- 1):
-        print()
-        print("-" * row_length)
-      else:
-        print()
+            print()
+
 
 def get_move(board):
-  """Получение хода игрока."""
-  size = len(board)
+    """Получение хода игрока."""
+    size = len(board)
 
-  while True:
-    move = input(f"Введите координаты хода (номер строки и столбца) через пробел от 1 до {size}: ")
-    
-    if len(move.split()) != 2:
-      print("Введите ДВА числа.")
-      continue
+    while True:
+        move = input(
+            f"Введите координаты хода (номер строки и столбца) через пробел от 1 до {size}: "
+        )
 
-    try:
-      # Преобразование введенных координат в индексы списка
-      row = int(move.split()[0]) - 1
-      col = int(move.split()[1]) - 1
+        if len(move.split()) != 2:
+            print("Введите ДВА числа.")
+            continue
 
-    except (ValueError, IndexError):
-      print("Неверно указаны координаты.")
-      continue
+        try:
+            # Преобразование введенных координат в индексы списка
+            row = int(move.split()[0]) - 1
+            col = int(move.split()[1]) - 1
 
-    if not (0 <= row < size and 0 <= col < size):
-      print("Неверно указаны координаты.")
-      continue
+        except (ValueError, IndexError):
+            print("Неверно указаны координаты.")
+            continue
 
-    if board[row][col] != " ":
-      print("Клетка занята!")
-      continue
+        if not (0 <= row < size and 0 <= col < size):
+            print("Неверно указаны координаты.")
+            continue
 
-    else:
-      return row, col
+        if board[row][col] != " ":
+            print("Клетка занята!")
+            continue
+
+        else:
+            return row, col
+
 
 def make_move(board, row, col, current_player):
-  """Выполнение хода игрока."""
-  board[row][col] = current_player
-  
+    """Выполнение хода игрока."""
+    board[row][col] = current_player
+
+
 def check_win(board, current_player):
-  """Проверка победы"""
+    """Проверка победы"""
 
-  # Проверка победного условия по строке  
-  for row in board:
+    # Проверка победного условия по строке
+    for row in board:
 
-    if all(cell == current_player for cell in row):
-      return True
+        if all(cell == current_player for cell in row):
+            return True
 
-  
-  # Проверка победного условия по столбцу
-  size = len(board)
+    # Проверка победного условия по столбцу
+    size = len(board)
 
-  for col_index in range(size):
-    
-    if all(board[row_index][col_index] == current_player for row_index in range(size)):
-      return True
-    
-    
-  # Проверка победного условия по основной диагонали
-  if  all(board[index][index] == current_player for index in range(size)):
-    return True
-  
+    for col_index in range(size):
 
-  # Проверка победного условия по побочной диагонали
-  if all(board[index][size-1-index] == current_player for index in range(size)):
-    return True
+        if all(
+            board[row_index][col_index] == current_player for row_index in range(size)
+        ):
+            return True
 
-  return False
+    # Проверка победного условия по основной диагонали
+    if all(board[index][index] == current_player for index in range(size)):
+        return True
+
+    # Проверка победного условия по побочной диагонали
+    if all(board[index][size - 1 - index] == current_player for index in range(size)):
+        return True
+
+    return False
 
 
 def check_draw(board):
-  """Проверка ничьей"""
-  
-  return all(cell != " " for row in board for cell in row)
+    """Проверка ничьей"""
+
+    return all(cell != " " for row in board for cell in row)
+
 
 def switch_player(current_player: str):
-  """Смена текущего игрока."""
-  
-  if current_player == "X":
-    return "O"
+    """Смена текущего игрока."""
 
-  else:
-    return "X"
+    if current_player == "X":
+        return "O"
+
+    else:
+        return "X"
+
 
 def run():
     """Запускает основной игровой цикл."""
@@ -122,23 +130,20 @@ def run():
     current_player = "X"
 
     while True:
-      draw_board(board)
-      print("\n" f"Ходит игрок {current_player}")
-      row, col = get_move(board)
-      make_move(board, row, col, current_player)
-      
-      if check_win(board, current_player):
-        print("\nПобедил игрок ", current_player)
         draw_board(board)
-        break
-        
-      
-      if check_draw(board):
-        print("Ничья!")
-        draw_board(board)
-        break
-        
+        print("\n" f"Ходит игрок {current_player}")
+        row, col = get_move(board)
+        make_move(board, row, col, current_player)
 
-      current_player = switch_player(current_player)
+        if check_win(board, current_player):
+            print("\nПобедил игрок ", current_player)
+            draw_board(board)
+            break
+
+        if check_draw(board):
+            print("Ничья!")
+            draw_board(board)
+            break
+
+        current_player = switch_player(current_player)
     input("Введите любую клавишу, чтобы выйти...")
-     
